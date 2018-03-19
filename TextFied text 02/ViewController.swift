b72de8f4f0ae76eb197caae62c976900b60e669e
D7703_08 <d7703_08@D7703-08ui-iMac.local>
2018-03-19 16:02:01 +0900

    find

--- TextFied text 02/ViewController.swift
+++ TextFied text 02/ViewController.swift
@@ -8,18 +8,37 @@
 
 import UIKit
 
-class ViewController: UIViewController {
-
+class ViewController: UIViewController, UITextFieldDelegate {
+    @IBOutlet weak var myTextFied: UITextField!
+    
+    
+    @IBOutlet weak var myLabel: UILabel!
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        //delrgate 연결
     }
-
+    
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
-
+    @IBAction func buttonPressed(_ sender: Any) {
+        
+       myLabel.text = myTextFied.text
+        myTextFied.text = ""
+    }
+    //UItextFiedDelegate metrod
+    //called when 'return'key pressed. teturn No to ignore
+    func textFieldShouldReturn(_ textFIeld: UITextField) -> Bool {
+        
+        print("I pressed return Key")
+        //키패드내림
+        myTextFied.resignFirstResponder()
+        return true
 
 }
 
+}
